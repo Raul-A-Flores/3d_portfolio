084e14aa2d74e964677629fde6c7fac8200d2ab5
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-11-11 13:04:57 -0800

    about me page added

---
+++ src/components/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CTA = () => {
+  return (
+    <section className='cta'>
+        <p className='cta-text'>Have a project in mind? <br  className='sm:block-hidden'/>Lets build something together</p>  
+        <Link className='btn' to='/contact'>
+        Contact
+        </Link>
+    </section>
+  )
+}
+
+export default CTA
